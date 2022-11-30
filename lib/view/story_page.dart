e36a2260edcb01ad60a62d814e54ce28c9dcf980
bishal.rumba007 <bishal.rumba007@gmail.com>
2022-11-30 16:01:19 +0545

    minor changes here

--- lib/view/story_page.dart
+++ lib/view/story_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get/get.dart';
@@ -7,7 +6,6 @@ import 'package:hacker_news_proj/view/web_view_page.dart';
 import '../provider/comment_provider.dart';
 import '../provider/story_provider.dart';
 import 'detail_page.dart';
-
 
 class StoryPage extends ConsumerWidget {
   const StoryPage({Key? key}) : super(key: key);
@@ -22,16 +20,16 @@ class StoryPage extends ConsumerWidget {
         centerTitle: true,
         actions: [
           IconButton(
-              onPressed: (){
+              onPressed: () {
+                // ignore: unused_result
                 ref.refresh(topStoryProvider);
               },
-              icon: const Icon(Icons.refresh)
-          )
+              icon: const Icon(Icons.refresh))
         ],
       ),
       body: SafeArea(
           child: storyData.when(
-              data: (data){
+              data: (data) {
                 return ListView.builder(
                   itemCount: data.length,
                   itemBuilder: (context, index) {
@@ -39,32 +37,28 @@ class StoryPage extends ConsumerWidget {
                       decoration: const BoxDecoration(
                           border: Border(
                               bottom: BorderSide(
-                                  color: Colors.black12,
-                              )
-                          )
-                      ),
-
+                        color: Colors.black12,
+                      ))),
                       child: ExpansionTile(
-                          title: Text(
-                            data[index].title!,
-                            style: const TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w500
-                            ),
-                          ),
+                        title: Text(
+                          data[index].title!,
+                          style: const TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w500),
+                        ),
                         subtitle: Text(
                           data[index].by!,
                           style: const TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w500
-                          ),
+                              fontSize: 14, fontWeight: FontWeight.w500),
                         ),
                         children: [
                           Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 15.0),
                             child: InkWell(
-                              onTap: (){
-                                ref.read(commentCall).getComment(kids: data[index].kids);
+                              onTap: () {
+                                ref
+                                    .read(commentCall)
+                                    .getComment(kids: data[index].kids);
                                 Get.to(() => DetailPage(news: data[index]));
                               },
                               child: SizedBox(
@@ -72,30 +66,33 @@ class StoryPage extends ConsumerWidget {
                                 width: double.infinity,
                                 child: Row(
                                   // crossAxisAlignment: CrossAxisAlignment.start,
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
                                   children: <Widget>[
                                     Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
                                       children: [
                                         Text(
                                           'id: ${data[index].id!}',
                                           style: const TextStyle(
                                               fontSize: 15,
-                                              fontWeight: FontWeight.w400
-                                          ),
+                                              fontWeight: FontWeight.w400),
                                         ),
                                         Text(
                                           'score: ${data[index].score!.toString()}',
                                           style: const TextStyle(
                                               fontSize: 15,
-                                              fontWeight: FontWeight.w400
-                                          ),
+                                              fontWeight: FontWeight.w400),
                                         ),
                                       ],
                                     ),
                                     Column(
                                       children: [
-                                        const Icon(Icons.message_outlined, color: Colors.black54,),
+                                        const Icon(
+                                          Icons.message_outlined,
+                                          color: Colors.black54,
+                                        ),
                                         // IconButton(
                                         //     onPressed: (){
                                         //       ref.read(commentCall).getComment(kids: data[index].kids);
@@ -103,7 +100,11 @@ class StoryPage extends ConsumerWidget {
                                         //     },
                                         //     icon: const Icon(Icons.message_outlined, color: Colors.black54,)
                                         // ),
-                                        Text(data[index].descendants.toString(), style: const TextStyle(fontSize: 14),)
+
+                                        Text(
+                                          data[index].descendants.toString(),
+                                          style: const TextStyle(fontSize: 14),
+                                        )
                                       ],
                                     )
                                   ],
@@ -114,25 +115,20 @@ class StoryPage extends ConsumerWidget {
                           ListTile(
                             title: Text(data[index].url.toString()),
                             trailing: IconButton(
-                                onPressed: (){
-                                  Get.to(() => WebViewApp(urlData: data[index].url!));
+                                onPressed: () {
+                                  Get.to(() =>
+                                      WebViewApp(urlData: data[index].url!));
                                 },
-                                icon: const Icon(Icons.navigate_next)
-                            ),
+                                icon: const Icon(Icons.navigate_next)),
                           ),
                         ],
                       ),
-                      
                     );
                   },
                 );
               },
               error: (err, stack) => Text('$err'),
-              loading: () => const Center(child: CircularProgressIndicator())
-          )
-      ),
+              loading: () => const Center(child: CircularProgressIndicator()))),
     );
   }
 }
-
-
